0ec67606923be742fc273c53e7545ca39b285272
Florian Tanner <florian.tanner@gmail.com>
2015-11-07 18:52:43 +0100

    added electron app

---
+++ evolutjs/matter-test.js
@@ -0,0 +1,35 @@
+console.log('hallo');
+
+var jQuery = require('jquery');
+
+jQuery(document).ready(function() {
+
+console.log('rdy start');
+
+  var Pixi = require('pixi');
+  var Matter = require('matter-js');
+
+  // Matter.js module aliases
+  var Engine = Matter.Engine,
+      World = Matter.World,
+      Bodies = Matter.Bodies;
+
+  // create a Matter.js engine
+  var engine = Engine.create(document.body);
+
+  // create two boxes and a ground
+  var boxA = Bodies.rectangle(400, 200, 80, 80);
+  var boxB = Bodies.rectangle(450, 50, 80, 80);
+  var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+  // add all of the bodies to the world
+  World.add(engine.world, [boxA, boxB, ground]);
+
+  // run the engine
+  Engine.run(engine);
+
+  console.log('rdy done');
+
+});
+
+console.log('eof');
