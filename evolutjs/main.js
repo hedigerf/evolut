52472d1e76290e37f016c66055e1e9f095c09c12
Fabian Hediger <hedigerf@gmail.com>
2016-02-18 13:56:24 +0100

    fixed electron build error. switched to prebuild electron compile

--- evolutjs/main.js
+++ evolutjs/main.js
@@ -1,3 +1,5 @@
 require('crash-reporter').start();
-require('electron-compile').init();
+//var appRoot = '/home/hedigerf/GitReps/evolut/evolutjs';
+//require('electron-compile').init(appRoot,'./main');
+require('electron-prebuilt-compile');
 require('./app');
