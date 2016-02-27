63edced162200c06fe55d481af8731c66361f4db
Fabian Hediger <hedigerf@gmail.com>
2016-02-27 14:49:36 +0100

    Tournamed Based strategy prototype

--- evolutjs/src/algorithm/selection/tournamentBasedSelectionStrategy.js
+++ evolutjs/src/algorithm/selection/tournamentBasedSelectionStrategy.js
@@ -8,36 +8,34 @@ const logger = log4js.getLogger('TournamentBasedSelectionStrategy');
 const random = new Random(Random.engines.mt19937().autoSeed());
 
 
-export class TournamentBasedSelectionStrategy extends SelectionStrategy{
-  constructor(population,k) {
+export class TournamentBasedSelectionStrategy extends SelectionStrategy {
+  constructor(population, k) {
     super(population);
     this.k = k;
     logger.info('Torunament-based Selection k = ' + k);
   }
 
   select() {
-    const runs = this.initalPopulation.size / this.k;
+    const runs = this.initalPopulation.size;
 
     const selectForRun = (runNr) => {
 
       const chooseIndividuals = (kCount) => {
-        const randomIndex = random.integer(0, this.initalPopulation.size - 1);
+        const randomIndex = random.integer(0, this.initalPopulation.size -
+          1);
         logger.debug('kCount: ' + kCount + ' randomIndex: ' + randomIndex);
         return this.initalPopulation.individuals[randomIndex];
       };
-      // TODO : Implement fittest selection
-      const chooseFittest = (individuals) => {return individuals[0];};
 
       logger.debug('RunNr: ' + runNr);
-      const selectedIndividuals = Immutable.Range(1,this.k).map(chooseIndividuals);
-      const fittestIndividual = chooseFittest(selectedIndividuals);
-      // TODO : Generate offspring with same genes
-      const offspring = fittestIndividual;
-
-      return offspring;
+      const selectedIndividuals = Immutable.Range(1, this.k).map(
+        chooseIndividuals);
+      const fittestIndividual = selectedIndividuals.max((individualA,
+        individualB) => individualA.fitness > individualB.fitness);
+      return fittestIndividual;
     };
 
-    Immutable.Range(1,runs).map(selectForRun);
+    Immutable.Range(1, runs).map(selectForRun);
 
   }
 
