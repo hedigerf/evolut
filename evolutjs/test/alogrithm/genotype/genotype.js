3872be5f921e657ca54f60803029d8da71f47e67
Florian Tanner <fta@gartenmann.ch>
2016-04-29 13:50:48 +0200

    Unit test für Genotype angepasst

--- evolutjs/test/alogrithm/genotype/genotype.js
+++ evolutjs/test/alogrithm/genotype/genotype.js
@@ -7,22 +7,42 @@ import chaiImmutable from 'chai-immutable';
 chai.use(chaiImmutable);
 
 describe('Genotype', () => {
+
+  describe('build', () => {
+    it('should be buildable', () => {
+      chai.expect(Genotype).itself.to.respondTo('build');
+    });
+  });
+
+  describe('mutate', () => {
+    it('should be mutatable', () => {
+      chai.expect(new Genotype()).to.respondTo('mutate');
+    });
+  });
+
   describe('seed', () => {
+    it('should be seedable', () => {
+      chai.expect(Genotype).itself.to.respondTo('seed');
+    });
     it('should have no parts', () => {
       chai.expect(Genotype.parts).to.be.empty;
     });
   });
+
 });
 
 describe('PartialGenotype', () => {
+
   describe('identifier', () => {
-    it('should have no implementation for identifier', () => {
-      chai.expect(() => PartialGenotype.identifier).to.throw(Error, 'not implemented');
+    it('should have an empty identifier', () => {
+      chai.expect(PartialGenotype.identifier).to.equal('');
     });
   });
+
   describe('seed', () => {
     it('should have no parts', () => {
       chai.expect(PartialGenotype.parts).to.be.empty;
     });
   });
+
 });
